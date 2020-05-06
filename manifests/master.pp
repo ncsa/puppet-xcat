954eead468dd124e140f523654b6bc48da33e8fe
Andy Loftus <aloftus@gmail.com>
2020-05-06 16:49:00 -0500

    Remove master::postscripts as a default include

--- manifests/master.pp
+++ manifests/master.pp
@@ -6,7 +6,6 @@ class xcat::master {
     include ::xcat::master::bmc_smtp
     include ::xcat::master::firewall
     include ::xcat::master::nfs
-    include ::xcat::master::postscripts
     include ::xcat::master::root
     include ::xcat::master::tcpwrappers
 }
